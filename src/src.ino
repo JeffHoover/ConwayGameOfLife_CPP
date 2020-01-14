b1fb8121efb6108f7ec1a365fae7aaed926fbe7a
Jeff Hoover <jeff.hoover@accenture.com>
2020-01-14 18:05:17 -0500

    reminder comment

--- src/src.ino
+++ src/src.ino
@@ -10,50 +10,51 @@
 // Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
 // Matrix Shield, cut trace between CLK pads and run a wire to A4).
 
-#define CLK  11   // USE THIS ON ADAFRUIT METRO M0, etc.
+#define CLK 11 // USE THIS ON ADAFRUIT METRO M0, etc.
 //#define CLK A4 // USE THIS ON METRO M4 (not M0)
 //#define CLK 11 // USE THIS ON ARDUINO MEGA
-#define OE   9
+#define OE 9
 #define LAT 10
-#define A   A0
-#define B   A1
-#define C   A2
-#define D   A3
+#define A A0
+#define B A1
+#define C A2
+#define D A3
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 64);
 
-void setup() {
+void setup()
+{
   matrix.begin();
   gameSetup();
-  
-
 }
 
-void loop() {
+void loop()
+{
 
-    bool *active;
-    active = (bool *)grid;
+  bool *active;
+  active = (bool *)grid;
 
-    display(active);
-    delay(5000);
+  display(active);
+  delay(5000);
 
-    active = (bool *)altGrid;
+  active = (bool *)altGrid;
 
-    display(active);
-    delay(5000);
-    
+  display(active);
+  delay(5000);
 }
 
 void display(bool *active)
 {
-  for(int x = 0; x < NUM_ROWS; x++)
-  {
-        for(int y = 0; y < NUM_COLS; y++)
-      {
-        bool *cellIsAlive = active+(x*NUM_COLS + y);
-        matrix.drawPixel(x, y, matrix.Color333(0 , 0, *cellIsAlive * 7));
+  // NOT TESTED
 
-      }
+  for (int x = 0; x < NUM_ROWS; x++)
+  {
+    for (int y = 0; y < NUM_COLS; y++)
+    {
+      bool *cellIsAlive = active + (x * NUM_COLS + y);
+      uint colorIntensity = *cellIsAlive *;
+      matrix.drawPixel(x, y, matrix.Color333(colorIntensity, colorIntensity, colorIntensity));
+    }
   }
-  matrix.drawPixel(2,2, matrix.Color333(7,0,0));
+  matrix.drawPixel(2, 2, matrix.Color333(7, 0, 0)); // Red pixel for orientation
 }
