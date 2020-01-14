c5f5808b188386b60d33f6b6bb0ca63adc5b4370
Jeff Hoover <jeff.hoover@accenture.com>
2020-01-14 17:27:04 -0500

    Refactoring tests

--- test/testneighbors.cpp
+++ test/testneighbors.cpp
@@ -2,106 +2,88 @@
 #include <gtest/gtest.h>
 #include "life.h"
 
+#define TEST_CELL_ROW 1
+#define TEST_CELL_COL 1
+
 using namespace ::testing;
 
-class neighbors : public ::testing::Test {
- protected:
-  void SetUp() override {
-     mockInit();
-  }
+class neighbors : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        mockInit();
+    }
 };
 
-TEST_F(neighbors, returnsOneIfOnlyRightNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyRightNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[1][2] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
 
-TEST_F(neighbors, returnsOneIfOnlyLeftNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyLeftNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[1][0] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
 
-TEST_F(neighbors, returnsTwoIfLeftAndRightNeighborsAlive) {
+TEST_F(neighbors, returnsTwoIfLeftAndRightNeighborsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[1][0] = true;
     grid[1][2] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 2);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 2);
 }
-TEST_F(neighbors, returnsOneIfOnlyTopNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyTopNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[0][1] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
-TEST_F(neighbors, returnsOneIfOnlyBottomNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyBottomNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[2][1] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
 
-TEST_F(neighbors, returnsOneIfOnlyTopLeftNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyTopLeftNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[0][0] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
 
-TEST_F(neighbors, returnsOneIfOnlyTopRightNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyTopRightNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[0][2] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
 
-TEST_F(neighbors, returnsOneIfOnlyBottomLeftNeighborIsAlive) {
+TEST_F(neighbors, returnsOneIfOnlyBottomLeftNeighborIsAlive)
+{
 
     memset(grid, false, sizeof(grid));
     grid[2][0] = true;
 
-    int row = 1;
-    int col = 1;
-
-    EXPECT_EQ(countNeighbors(row, col), 1);
-
+    EXPECT_EQ(countNeighbors(TEST_CELL_ROW, TEST_CELL_COL), 1);
 }
+
