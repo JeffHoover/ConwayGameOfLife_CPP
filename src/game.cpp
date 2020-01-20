4a610971e7a90388fb9f44869f3fd77879a8830f
Jeff Hoover <jeff.hoover@accenture.com>
2020-01-19 22:37:32 -0500

    remove debugging printf

--- src/game.cpp
+++ src/game.cpp
@@ -22,18 +22,6 @@ void gameSetup()
 
 void gameLoop()
 {
-    int aliveCount = 0;
-    for (int ii = 0; ii < NUM_COLS; ii++)
-    {
-        for (int jj = 0; jj < NUM_ROWS; jj++)
-            if (futureGeneration->get(ii, jj) == ALIVE)
-            {
-                aliveCount++;
-            }
-    }
-    printf(" ZZ before aliveCount %d", aliveCount);
-    fflush(stdout);
-
     for (uint8_t x = 0; x < NUM_COLS; ++x)
     {
         for (uint8_t y = 0; y < NUM_ROWS; ++y)
@@ -41,49 +29,13 @@ void gameLoop()
             bool state = currentGeneration->get(x, y);
             int liveNeighborCount = currentGeneration->countAliveNeighbors(x, y);
             bool nextState = nextStateIsAlive(state, liveNeighborCount);
-            if (nextState == ALIVE)
-            {
-                printf(" LE %d,%d [state %d] %d live neighbors. Will become %d\n", x, y, state, liveNeighborCount, nextState);
-                fflush(stdout);
-            }
-
-            // if (state == ALIVE || liveNeighborCount == 3)
-            // {
-            //     printf(" LE %d,%d [state %d] %d live neighbors. Will become %d\n", x, y, state, liveNeighborCount, nextState);
-            //     fflush(stdout);
-            // }
             futureGeneration->set(x, y, nextState);
         }
     }
 
-    aliveCount = 0;
-    for (int ii = 0; ii < NUM_COLS; ii++)
-    {
-        for (int jj = 0; jj < NUM_ROWS; jj++)
-            if (futureGeneration->get(ii, jj) == ALIVE)
-            {
-                aliveCount++;
-            }
-    }
-    printf(" ZZ after aliveCount %d\n", aliveCount);
-    fflush(stdout);
-
     tmpGeneration = futureGeneration;
     futureGeneration = currentGeneration;
     currentGeneration = tmpGeneration;
-
-    aliveCount = 0;
-    for (int ii = 0; ii < NUM_COLS; ii++)
-    {
-        for (int jj = 0; jj < NUM_ROWS; jj++)
-            if (currentGeneration->get(ii, jj) == ALIVE)
-            {
-                aliveCount++;
-                printf("ZZ [%d,%d]   ", ii, jj);
-            }
-    }
-    printf(" ZZ after swap aliveCount %d\n", aliveCount);
-    fflush(stdout);
 
     display();
 }
--- src/rules.cpp
+++ src/rules.cpp
@@ -1,31 +1,14 @@
 #include "rules.h"
-#include "stdio.h" // for printf
 
 bool nextStateIsAlive(bool cellIsAlive, int numberOfAliveNeighbors)
 {
-    if (cellIsAlive || numberOfAliveNeighbors == 3)
-    {
-        printf("\n\na %d LE liveNeighbors --- ", numberOfAliveNeighbors);
-    }
     if (numberOfAliveNeighbors == 2 && !cellIsAlive)
     {
-        if (cellIsAlive)
-        {
-            printf("LE b\n");
-        }
         return DEAD;
     }
     if (numberOfAliveNeighbors > 1 && numberOfAliveNeighbors < 4)
     {
-        if (cellIsAlive)
-        {
-            printf("LE d\n");
-        }
         return ALIVE;
-    }
-    if (cellIsAlive)
-    {
-        printf("LE e %d \n", numberOfAliveNeighbors);
     }
     return DEAD;
 }
